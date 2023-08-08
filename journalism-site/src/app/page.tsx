0571cac2a1018414b307d1e48491f6f1542bf4bb
henrymutua <mutuahenry116@gmail.com>
2023-08-08 15:19:39 +0300

    added the main components for the news site

---
+++ journalism-site/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      
+    </div>
+  )
+}
---
+++ journalism-site/src/components/NavMenu/NavMenu.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './NavMenu.module.css'
+
+
+const NavMenu = () => {
+    return(
+        <div className={styles.NavMenu}>
+
+        </div>
+    )
+}
+
+export default NavMenu;
